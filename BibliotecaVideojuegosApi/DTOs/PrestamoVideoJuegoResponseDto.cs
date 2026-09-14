48edd6eb8c2023feff4b9e989a36f0f607646b33
Daiana Di Stefano <daiana_d.s@hotmail.com>
2026-09-14 15:59:43 -0300

    feat: agrego DTOs de prestamos

---
+++ BibliotecaVideojuegosApi/DTOs/PrestamoVideoJuegoResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using BibliotecaVideojuegosApi.Models;
+using System.ComponentModel.DataAnnotations;
+namespace BibliotecaVideojuegosApi.DTOs;
+
+public class PrestamoVideoJuegoResponseDto
+{
+    public int CopiaVideoJuegoId { get; set; }
+    public DateTime FechaPrestamo { get; set; }
+    public DateTime? FechaDevolucion { get; set; }
+    public string NombreUsuario { get; set; } = null!;
+}
