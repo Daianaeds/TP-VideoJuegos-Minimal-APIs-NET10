bf95624c15bed249cd32fe2899b6d10d7f3188cf
Daiana Di Stefano <daiana_d.s@hotmail.com>
2026-09-14 23:35:00 -0300

    fix: modifico el dto de copias de videos

--- BibliotecaVideojuegosApi/DTOs/CopiasVideosJuegosResponseDto.cs
+++ BibliotecaVideojuegosApi/DTOs/CopiasVideosJuegosResponseDto.cs
@@ -2,6 +2,7 @@
 
 public class CopiasVideosJuegosResponseDto
 {
+    public int Id { get; set; }
     public int VideoJuegoId { get; set; }
     public bool EstadoPrestado { get; set; }
     public VideoJuegoResponseDto? VideoJuego { get; set; }
